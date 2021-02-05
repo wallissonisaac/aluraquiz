9d1bea5f8113aa73bcb670a9143924892e9dccc1
Wallisson <wallissonisaac@gmail.com>
2021-02-05 02:04:45 -0300

    Página de Respostas e Layout das Altenativas

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,6 @@ export default function Home() {
             <form onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
-              console.log('Fazendo uma submissão por meio do React');
             }}
             >
               <Input
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import React from 'react';
 
 // src/components/Footer/index.js
 const FooterWrapper = styled.footer`
@@ -33,7 +34,7 @@ export default function Footer(props) {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante
+        Criado por Wallisson Isaac durante
         {' '}
         a
         {' '}
